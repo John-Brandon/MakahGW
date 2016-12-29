3860943df15b40cca3cd5653efbaa07eb14893d3
John Brandon <jbrandon@gmail.com>
2016-12-29 12:17:17 -0800

    First Bash scripts for batching trial simulations

---
+++ bash/run.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Bottom level script that runs simulations for a given trial.
+# The trial ID is given by the mid-level script that calls run.sh
+# At the top of the script hierarchy is the upper level script, that 
+#+   batches sets of trial runs. 
+# This low level script also calls programs to process simulation output,
+#+   and organize that output into the appropriate files and directories. 
+# Commands were ported from DOS script by Andre Punt / Cherry Allison, to
+#+   bash script here by John Brandon. 
+# -----------------------------------------------------------------------
+
+set -x
+
+# cd ~/MakahGW/f90/Project
+# ~/MakahGW/bash
+cd ../f90/Project
+
+echo 'Hello from run.sh'
+echo $PWD
+
+rm copy.dat;
+rm aw-resI;
+rm aw-resZ;
+rm aw-res;
+
+echo "Running Trial G$1"
+
+#cp ./Runstreams/G$1.DDD ./copy.dat
+cp ./Runstreams/GB01a.DDD ./copy.dat;
+
+cp manage.z manage.dat;
+./main.app | tee a.0;
+
+cp manage.i manage.dat;
+# ./main.app > a.0;
+./main.app | tee a.0;
+
+#cp manage.$2 manage.dat;
+cp manage.1 manage.dat;
+./main.app | tee a.1;
+
+./aw-res7.app > a.2;  # This doesn't seem necessary
+
+./proc.app > ../AllOut/figf$2$1.out;
+
+cp aw-traj.out ../AllOut/aw-traj$2.$1;
+cp aw-conf.out ../AllOut/aw-conf$2.$1;
+cp aw-sum.out ../AllOut/aw-sum$2.$1;
+
+
+cd ~/MakahGW/bash
+# cd ../bash
+
+#
+##rm aw-resI
+##rm aw-resZ
+##rm aw-res
+#
+#rm aw-traj-out;
+#rm aw-sum.out;
+#rm *.20;
+#rm *.100;
+#rm *.res;
+#rm aw-table;
+#rm aw-1-2.out;
+#rm aw-conf.out;
+
+
+# MS DOS version ----------------------------------------------------------
+# del copy.dat
+# del aw-resI
+# del aw-resZ
+# del aw-res
+# copy runstreams\G%1.DDD copy.dat
+# copy manage.z manage.dat
+# f2-gup2 >a.0
+# copy manage.i manage.dat
+# f2-gup2 >a.0
+# copy manage.%2 manage.dat
+# f2-gup2 >a.1
+# aw-res7 >a.2
+# proc > ..\ALLOUT\figout%2.%1
+# copy aw-traj.out ..\ALLOUT\aw-traj%2.%1
+# copy aw-conf.out ..\ALLOUT\aw-conf%2.%1
+# copy aw-sum.out ..\ALLOUT\aw-sum%2.%1
+# #copy aw-resZ ..\ALLOUT\aw-resZ%2.%1
+# #del aw-resI
+# #del aw-resZ
+# #del aw-res
+# del aw-traj.out
+# del aw-sum.out
+# del *.20
+# del *.100
+# del *.res
+# del aw-table
+# del aw-1-2.out
+# del aw-conf.out
+
+
---
+++ bash/runset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# verbose 
+set -x
+
+# echo 'Hello from runset.sh'
+# echo $PWD
+
+#source run.sh $1 1
+
+source run.sh B01A 1
+source run.sh B01B 1
+source run.sh B01C 1
+source run.sh B01D 1
+# 
+# source run.sh I01A 1
+# source run.sh I01B 1
+# source run.sh I01C 1
+# source run.sh I01D 1
+# 
+# source run.sh P01A 1
+# source run.sh P01B 1
+# source run.sh P01C 1
+# source run.sh P01D 1
+
+
+# call runtst $1 2
+# call runtst $1 3
+# call runtst $1 4
+# call runtst $1 5
+# call runtst $1 6
+# call runtst $1 7
+# call runtst $1 8
+# call runtst $1 9
+# call runtst $1 10
